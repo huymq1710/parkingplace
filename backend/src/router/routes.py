f72a15ceee27f5d50a1c3d9c48cfb6d7bf8fd15e
Huy Mạc Quang <57699454+huymq1710@users.noreply.github.com>
2020-12-28 00:12:04 +0700

    Revert "merge"

--- backend/src/router/routes.py
+++
@@ -1,9 +0,0 @@
-from src.api.LocationApi import LocationApi
-from src.api.ParkingSlotApi import ParkingSLotApi
-
-def initialize_routes(api):   
-    #location
-    api.add_resource(LocationApi, '/location')
-    
-    #slot
-    api.add_resource(ParkingSLotApi, '/parkingslot')
